e8dbf8f962fc9f985da82444cad59d2d58106e1c
adam.palmkvist1@outlook.com <adam.palmkvist1@outlook.com>
2022-06-15 16:51:32 +0200

    add target

---
+++ target/rls/debug/deps/proc_macro2-a5e425f7120e3c29.d
@@ -0,0 +1,12 @@
+c:\picolo\target\rls\debug\deps\proc_macro2-a5e425f7120e3c29.rmeta: C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\lib.rs C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\marker.rs C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\parse.rs C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\detection.rs C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\fallback.rs C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\wrapper.rs
+
+c:\picolo\target\rls\debug\deps\libproc_macro2-a5e425f7120e3c29.rlib: C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\lib.rs C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\marker.rs C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\parse.rs C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\detection.rs C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\fallback.rs C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\wrapper.rs
+
+c:\picolo\target\rls\debug\deps\proc_macro2-a5e425f7120e3c29.d: C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\lib.rs C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\marker.rs C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\parse.rs C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\detection.rs C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\fallback.rs C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\wrapper.rs
+
+C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\lib.rs:
+C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\marker.rs:
+C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\parse.rs:
+C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\detection.rs:
+C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\fallback.rs:
+C:\Users\adamp\.cargo\registry\src\github.com-1ecc6299db9ec823\proc-macro2-1.0.39\src\wrapper.rs:
