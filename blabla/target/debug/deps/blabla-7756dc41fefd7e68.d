c67a2715d74cacac1a7edd36275d5505ef74e81b
Adam <m123.123.123@hotmail.com>
2022-07-01 02:03:51 +0200

    remove excess file

--- blabla/target/debug/deps/blabla-7756dc41fefd7e68.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Adamx\picolo\blabla\target\debug\deps\blabla-7756dc41fefd7e68.rmeta: src\main.rs
-
-c:\Users\Adamx\picolo\blabla\target\debug\deps\blabla-7756dc41fefd7e68.d: src\main.rs
-
-src\main.rs:
--- blabla/target/debug/deps/blabla-b9aced5fe4e9c7f8.d
+++
@@ -1,5 +0,0 @@
-c:\Users\Adamx\picolo\blabla\target\debug\deps\blabla-b9aced5fe4e9c7f8.rmeta: src\main.rs
-
-c:\Users\Adamx\picolo\blabla\target\debug\deps\blabla-b9aced5fe4e9c7f8.d: src\main.rs
-
-src\main.rs:
