24ba7d68f950f22dbecfb6b5b0895b9381adb195
Adam <adam@pop-os.localdomain>
2022-07-09 17:28:05 +0200

    comment add

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use picolo::plot::plot_tbl;
 use picolo::readimg::*;
 mod tests;
 
+// Example use:
 fn main() {
     let x = vec![0, 152, 142, 500, 50, 566];
     let y = vec![0, 152, 100, 600, 50, 900];
