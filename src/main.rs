85569a3e5d5fd5a45d18619d0f409b8026a71ec3
Adam <m123.123.123@hotmail.com>
2022-07-09 17:47:51 +0200

    third attempt

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use picolo::plot::plot_tbl;
 use picolo::readimg::*;
 mod tests;
 
-// Exampl usage:
+// Example usage:
 fn main() {
     let x = vec![0, 152, 142, 500, 50, 566];
     let y = vec![0, 152, 100, 600, 50, 900];
